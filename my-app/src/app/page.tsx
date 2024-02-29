eeb69f84ce324f4495e2514928c4e15943d960c5
viponcio <vitoria.poncio@gmail.com>
2024-02-29 16:18:52 -0300

    tabela pronta

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,11 @@
-import Header from "./Table";
+import Header from "./Header";
+import Row from "./Row";
 
 export default function Home() {
-  return (
-    <main className="">
-      <Header />
-    </main>
-  );
+	return (
+		<main className="">
+			<Header />
+			<Row />
+		</main>
+	);
 }
